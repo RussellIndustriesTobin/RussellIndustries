a72c5e31097fb63262a16aea97dd97dbce85a6ce
RussellIndustriesTobin <tobin.tojo@russellindustries.com>
2024-06-28 15:35:35 -0400

    Add files via upload

--- support.js
+++ support.js
@@ -6,7 +6,7 @@ ticketBtn.addEventListener("click", function(){
     console.log(text); // Display the plain text content    
     this.classList.toggle('clicked');
     var img = document.createElement('img');
-    img.src = './images/check.png'; // Replace with your image path
+    img.src = './images/white-check.png'; // Replace with your image path
     img.style.width = '100%';
     img.style.height = 'auto';
     this.innerHTML = '';
